4a663d6f7ead7306231bf2df752bd0a364fb964f
cdne <cduneata@gmail.com>
2019-12-10 11:37:23 +0200

    Add pep8 format

--- data_manager.py
+++ data_manager.py
@@ -7,17 +7,18 @@ ANSWERS = 'data/answers.csv'
 question_list = connection.read_questions(QUESTIONS)
 answers_list = connection.read_answers(ANSWERS)
 
+
 def add_question():
     title = request.form['title']
     message = request.form['message']
-    new_question = { 'id': len(question_list),
-                'submission_time': '',
-                'view_number': 0,
-                'vote_number': 0,
-                'title': title,
-                'message': message,
-                'image': ''
-    }
+    new_question = {'id': len(question_list),
+                    'submission_time': '',
+                    'view_number': 0,
+                    'vote_number': 0,
+                    'title': title,
+                    'message': message,
+                    'image': ''
+                    }
 
     question_list.append(new_question)
     connection.write_questions(QUESTIONS, question_list)
