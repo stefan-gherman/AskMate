6c1c625b5d14f2183cb7fb45d23c6059f42a900a
cdne <cduneata@gmail.com>
2019-12-10 21:08:34 +0200

    Add vote question Refactor code

--- data_manager.py
+++ data_manager.py
@@ -3,12 +3,12 @@ import connection as connection
 QUESTIONS = 'data/questions.csv'
 ANSWERS = 'data/answers.csv'
 
-question_list = connection.read_questions(QUESTIONS)
+questions_list = connection.read_questions(QUESTIONS)
 answers_list = connection.read_answers(ANSWERS)
 
 
 def add_question(title, message):
-    new_question = {'id': len(question_list),
+    new_question = {'id': len(questions_list),
                     'submission_time': '',
                     'view_number': 0,
                     'vote_number': 0,
@@ -16,9 +16,8 @@ def add_question(title, message):
                     'message': message,
                     'image': ''
                     }
-    question_list.append(new_question)
-    connection.write_questions(QUESTIONS, question_list)
-    return 3
+    questions_list.append(new_question)
+    connection.write_questions(QUESTIONS, questions_list)
 
 
 def add_answer(question_id, message):
@@ -33,10 +32,22 @@ def add_answer(question_id, message):
 
 
 def vote_question(question_id, option):
-    vote_number = question_list[int(question_id)]['vote_number']
+    vote_number = questions_list[int(question_id)]['vote_number']
     if option == 'vote_up':
-        question_list[int(question_id)]['vote_number'] = int(vote_number) + 1
-        connection.write_questions(QUESTIONS, question_list)
+        questions_list[int(question_id)]['vote_number'] = int(vote_number) + 1
+        connection.write_questions(QUESTIONS, questions_list)
     elif option == 'vote_down':
-        question_list[int(question_id)]['vote_number'] = int(vote_number) - 1
-        connection.write_questions(QUESTIONS, question_list)
+        questions_list[int(question_id)]['vote_number'] = int(vote_number) - 1
+        connection.write_questions(QUESTIONS, questions_list)
+
+
+def vote_answer(answer_id, option):
+    vote_number = answers_list[int(answer_id)]['vote_number']
+
+    if option == 'vote_up':
+        answers_list[int(answer_id)]['vote_number'] = int(vote_number) + 1
+        connection.write_answers(ANSWERS, answers_list)
+    elif option == 'vote_down':
+        answers_list[int(answer_id)]['vote_number'] = int(vote_number) - 1
+        connection.write_answers(ANSWERS, answers_list)
+    print(answers_list)
