c1d4b3677115c4ddfe5962a6fdd33d9c95712520
cdne <cduneata@gmail.com>
2019-12-10 10:11:51 +0200

    Add render_template, redirect, request

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 
 @app.route('/list')
 def route_index():
-    pass
+    return render_template('index.html')
 
 
 @app.route('/question/<question_id>')
