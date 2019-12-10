44ca647e1fa94b4bfba738b45bf37ba358ae4a69
cdne <cduneata@gmail.com>
2019-12-10 11:43:01 +0200

    Add return question id

--- data_manager.py
+++ data_manager.py
@@ -19,7 +19,6 @@ def add_question():
                     'message': message,
                     'image': ''
                     }
-    return new_question['id']
-
     question_list.append(new_question)
     connection.write_questions(QUESTIONS, question_list)
+    return new_question['id']
