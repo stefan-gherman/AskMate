49f1f87aefafc6d9d6f74221861c586ad3c4625b
Stefan Gherman <scgheman@gmail.com>
2019-12-12 05:52:05 +0200

    Fix for an error regarding the routes on index page.

--- server.py
+++ server.py
@@ -6,19 +6,22 @@ import util as util
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/list')
-def route_index():
-    FILE = 'data/questions.csv'
-    questions = connection.read_questions(FILE)
-    return render_template('index.html', questions=questions)
+# @app.route('/')
+# @app.route('/list')
+# def route_index():
+#     FILE = 'data/questions.csv'
+#     questions = connection.read_questions(FILE)
+#     return render_template('index.html', questions=questions)
 
-
+update_views = True
 @app.route('/question/<question_id>', methods=['GET', 'POST'])
 def route_question(question_id):
+    global update_views
     questions = connection.read_questions('data/questions.csv')
-    questions[int(question_id)]['view_number'] = int(questions[int(question_id)]['view_number']) + 1
-    connection.write_questions('data/questions.csv', questions)
+    print(update_views)
+    if update_views == True:
+        questions[int(question_id)]['view_number'] = int(questions[int(question_id)]['view_number']) + 1
+        connection.write_questions('data/questions.csv', questions)
     for elem in range(len(questions)):
         for key in questions[elem].keys():
             if question_id == questions[elem]['id']:
@@ -30,28 +33,35 @@ def route_question(question_id):
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def route_add_question():
+    global update_views
     if request.method == 'POST':
         title = request.form['title']
         message = request.form['message']
         data_manager.add_question(title, message)
-        return redirect(url_for("route_question", question_id=len(data_manager.questions_list) - 1))
+        questions_list = connection.read_questions(data_manager.QUESTIONS_FILE)
+        update_views = True
+        return redirect(url_for("route_question", question_id=str(len(questions_list) - 1)))
     else:
         return render_template('add_question.html')
 
 
 @app.route('/question/<question_id>/edit')
 def route_edit_question(question_id):
+    global update_views
+    update_views = False
     pass
 
 
 @app.route('/question/<question_id>/delete')
 def route_delete_question(question_id):
     questions = data_manager.delete_question(question_id)
-    return render_template('index.html', questions=questions)
+    return redirect(request.referrer)
 
 
 @app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
 def route_add_answer(question_id):
+    global update_views
+    update_views = False
     if request.method == 'POST':
         message = request.form['message']
         data_manager.add_answer(question_id, message)
@@ -61,6 +71,8 @@ def route_add_answer(question_id):
 
 @app.route('/answer/<answer_id>/delete')
 def route_delete_answer(answer_id):
+    global update_views
+    update_views = False
     answers = data_manager.delete_answer(answer_id)
     return redirect(request.referrer)
 
@@ -79,18 +91,24 @@ def route_question_vote_down(question_id):
 
 @app.route('/answer/<answer_id>/vote_up')
 def route_answer_vote_up(answer_id):
+    global update_views
+    update_views = False
     data_manager.vote_answer(answer_id, 'vote_up')
     return redirect(request.referrer)
 
 
 @app.route('/answer/<answer_id>/vote_down')
 def route_answer_vote_down(answer_id):
+    global update_views
+    update_views = False
     data_manager.vote_answer(answer_id, 'vote_down')
     return redirect(request.referrer)
 
-
-@app.route('/test_this', methods=['GET', 'POST'])
-def route_test_this():
+@app.route('/')
+@app.route('/list', methods=['GET', 'POST'])
+def route_index():
+    global update_views
+    update_views = True
     question_headers = connection.return_questions_headers()
     if request.method == 'GET':
         print(request.values.get('param'), request.values.get('sort_ord'))
