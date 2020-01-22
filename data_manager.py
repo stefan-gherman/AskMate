5e61463ace13f58e6fd25f349c1ff7e99b0e3af8
dandumitriu33 <dandumitriu33@gmail.com>
2020-01-22 10:01:06 +0200

    Resolve issue #89 edit/delete in search results

--- data_manager.py
+++ data_manager.py
@@ -394,7 +394,7 @@ def delete_tag_questions(cursor, question_id, tag_id_to_delete):
 def search_for_phrase(cursor, phrase_for_query):
     cursor.execute(
         sql.SQL(
-            "SELECT * from {table} WHERE to_tsvector('simple_english', {col1}) @@ to_tsquery('simple_english', %s) OR to_tsvector('simple_english', {col2}) @@ to_tsquery('simple_english', %s) ORDER BY {col3} desc;")
+            "SELECT {table}.*, person.username from {table} JOIN person ON question.user_id=person.id WHERE to_tsvector('simple_english', {col1}) @@ to_tsquery('simple_english', %s) OR to_tsvector('simple_english', {col2}) @@ to_tsquery('simple_english', %s) ORDER BY {col3} desc;")
             .format(table=sql.Identifier('question'),
                     col1=sql.Identifier('title'),
                     col2=sql.Identifier('message'),
